1b0f7c2d2a49face2300a9853ef44f668b397158
Wim Van de Meerssche <wim.vandemeerssche@intec.ugent.be>
2020-10-08 16:47:11 +0200

    quick untested draft of add issues functionality

--- ghtt/config.py
+++ ghtt/config.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import csv
 import re
-from typing import List, Dict
+from typing import List, Dict, Optional
 from urllib.parse import urlparse
 
 from jinja2 import Template
@@ -9,14 +9,17 @@ import yaml
 
 
 class Student:
-    username : str
-    comment : str
-    record : dict
-    group : str
+    username: str
+    comment: str
+    record: dict
+    group: str
 
     def __init__(self, username: str):
         self.username = username
+        self.fullname = ""
+        self.email = ""
         self.comment = ""
+        self.mentor_username = None
         self.record = {}
         self.group = None
 
@@ -29,9 +32,9 @@ class StudentRepo:
     def __init__(self, name: str):
         self.name = name
         self.comment = ""
-        self.students = []
+        self.students: List[Student] = []
+        self.mentors: List[Student] = []
         self.organization = ""
-
 
 
 def get(keypath: str, default):
@@ -48,7 +51,7 @@ def get(keypath: str, default):
     return default
 
 
-def get_students(usernames: List[str]=[], groups: List[str]=[]) -> List[Student]:
+def get_students(usernames: List[str] = [], groups: List[str] = []) -> List[Student]:
     students = []
     student_config = get("students", None)
     if not student_config:
@@ -58,12 +61,14 @@ def get_students(usernames: List[str]=[], groups: List[str]=[]) -> List[Student]
         with open(student_config["source"]) as f:
             rows = csv.DictReader(f, delimiter=',', quotechar='|')
             for row in rows:
-                student = Student(row[mapping['username']])
+                student = Student(row[mapping['username']], )
                 student.username = student.username.strip("#")
                 if usernames and student.username not in usernames:
                     continue
                 student.record = row
                 student.comment = Template(mapping["comment"]).render(record=student.record)
+                student.fullname = Template(mapping["fullname"]).render(record=student.record)
+                student.email = row[mapping['email']]
                 if mapping.get("group"):
                     student.group = student.record[mapping['group']]
                     student.group = student.group.lower()
@@ -79,11 +84,36 @@ def get_students(usernames: List[str]=[], groups: List[str]=[]) -> List[Student]
     return students
 
 
+def get_mentors() -> List[Student]:
+    """
+    Assumes config contains:
+     mentors:
+        - fullname: Person 1
+          username: pers1
+          email: pers1@example.com
+        - fullname: Person 2
+          username: pers2
+          email: pers2@example.com
+    """
+    mentors_config = get("mentors", None)
+    if not mentors_config:
+        return []
+    mentors = []
+    for m in mentors_config:
+        mentor = Student(m.get('username'))
+        mentor.fullname = m.get('fullname')
+        mentor.email = m.get('email')
+        mentors.append(mentor)
+    return mentors
+
+
 def get_organization() -> str:
     return urlparse(get("url", None)).path.rstrip("/").rsplit("/", 1)[-1]
 
 
-def get_repos(students:List[Student]) -> Dict[str, StudentRepo]:
+def get_repos(students: List[Student], mentors: Optional[List[Student]] = None) -> Dict[str, StudentRepo]:
+    if mentors is None:
+        mentors = []
     repos = {}
     student_config = get("students", None)
     if not student_config:
@@ -110,5 +140,6 @@ def get_repos(students:List[Student]) -> Dict[str, StudentRepo]:
         repo.students.append(student)
         repo.comment = ", ".join([s.comment for s in repo.students])
         repo.organization = organization
+        repo.mentors = mentors
         repos[repo.name] = repo
     return repos
