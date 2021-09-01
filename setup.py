b91d955ee25c43c72cd14c69fae40aa7ddb9c686
Wim Van de Meerssche <wim.vandemeerssche@intec.ugent.be>
2021-09-01 13:53:06 +0200

    fixed setup.py
    setup.py now reads requirements.txt so that it can never become out of date again

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@
 import os
 from setuptools import setup, find_packages
 
+
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(
     name="ghtt",
@@ -36,6 +38,6 @@ setup(
         "Programming Language :: Python :: 3.5",
         "License :: OSI Approved :: GNU Affero General Public License v3",
     ],
-    install_requires=["github3.py", "pygithub", "click", "requests", "jinja2", "pyyaml", "tabulate", "natsorted"],
+    install_requires=[r for r in read("requirements.txt").split('\n') if r.strip()],
     python_requires='>=3.5',
 )
